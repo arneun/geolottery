d421baa0218f9dbdb92070ae0e0a0b9e5a9fdf3e
Arneun <arneun@Karmagedon.home>
2018-11-24 19:32:01 +0100

    user and bet addition

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 
 import sqlite3
 from user import User
-
+from bet import Bet
 
 class Database:
 
@@ -15,7 +15,8 @@ class Database:
         
         c.execute('''CREATE TABLE prizes (lottery_time text, prize integer ) '''  )
         conn.commit()
-
+        c.execute('''CREATE TABLE bets (id, latitude real, longtitude real, ticket_type integer, timestamp text, user_id integer) ''')
+        conn.commit()
         conn.close()
     
     def authenticate(self, user_name, user_password):
@@ -36,7 +37,32 @@ class Database:
         conn.commit()
         conn.close()
         return res[0]
+    
+    def get_newest_bet(self):
+        conn = sqlite3.connect('/home/.geolottery_storage')
+        c = conn.cursor()
+        c.execute('''SELECT MAX(id) FROM bets''')
+        res = c.fetchall()
+        conn.commit()
+        conn.close()
+        return res[0]
+    
+
+    def add_bet(self, bet):
+        conn = sqlite3.connect('/home/.geolottery_storage')
+        c = conn.cursor()
+        c.execute('''INSERT INTO bets (id, latitude, longitude,ticket_type,timestamp ,user_id) VALUES (?,?,?,?,?)''', (get_newest_bet()+1, bet.latitude, bet.longitude, bet.ticket_type, bet.timestamp, bet.user))
+        conn.commit()
+        conn.close()
+    
 
     def add_user(self, user):
-        pass
+        conn = sqlite3.connect('/home/.geolottery_storage')
+        c = conn.cursor()
+        c.execute('''INSERT INTO users (numer, name, email, password ) VALUES (?,?,?,?)''', (get_newest_user() +1, user.name, user.email, user.password))
+        conn.commit()
+        conn.close()
+    
+
+
 
