57625830bebcc345fbcbfeb53817309247c7c54f
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2018-11-24 21:14:59 +0100

    Fixed price manager one last time

--- price_controller.py
+++ price_controller.py
@@ -3,7 +3,10 @@ from database import Database
 from flask import jsonify
 
 class PriceController:
- 
+
+    def __init__(self):
+        self.connection = Database()
+
     def get_prices(self):
         prices = self.connection.get_prices()
         return jsonify(prices)
