747fdd4705202948f554fefb515fbf36eadc39da
Arneun <arneun@Karmagedon.home>
2018-11-24 18:39:15 +0100

    get biggest user number

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ class Database:
     def setup(self):
         conn = sqlite3.connect('/home/.geolottery_storage')
         c = conn.cursor()
-        c.execute('''CREATE TABLE user (name text, mail text, password text) ''')
+        c.execute('''CREATE TABLE user (number integer, name text, mail text, password text) ''')
         conn.commit()
         c.execute('''CREATE TABLE prices (size integer, price integer)''')
         conn.commit()
@@ -21,12 +21,22 @@ class Database:
     def authenticate(self, user_name, user_password):
         conn = sqlite3.connect('/home/.geolottery_storage')
         c = conn.cursor()
-        c.execute('''SELECT name, FROM user WHERE name=? AND mail=? ''', (user_name, user_password) )
+        c.execute('''SELECT name, number FROM user WHERE name=? AND mail=? ''', (user_name, user_password) )
         res = c.fetchall()
         conn.commit()
         conn.close()
 
         return User(res[0], res[1], res[2])
         
+    def get_newest_user(self)
+        conn = sqlite3.connect('/home/.geolottery_storage')
+        c = conn.cursor()
+        c.execute('''SELECT MAX(number) FROM user''', (user_name, user_password) )
+        res = c.fetchall()
+        conn.commit()
+        conn.close()
+        return res[0]
 
+    def add_user(self, user):
+        pass
 
