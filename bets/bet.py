c26ddbe5b9c0aa9fbfd3dba4c621b7aeb05046fc
Burdzi0 <burdzi0@programmer.net>
2018-11-24 16:23:17 +0100

    Create whole bet module

---
+++ bets/bet.py
@@ -0,0 +1,10 @@
+
+
+class Bet:
+
+    def __init__(self, latitude, longitude, ticket_type, timestamp):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.ticket_type = ticket_type
+        self.timestamp = timestamp
+
---
+++ bets/bet_controller.py
@@ -0,0 +1,17 @@
+import map.country_checker as country_checker
+from bets.bet_manager import BetManager
+from flask import abort
+
+
+class BetController:
+
+    def __init__(self, bet_manager):
+        self.bet_manager = bet_manager
+
+    def handleRequest(self, x_coordinate, y_coordinate, ticket_type):
+        checker = country_checker.CountryChecker()
+
+        if checker.check_if_in_poland(x_coordinate, y_coordinate):
+            # Here u should register bet here
+            return self.bet_manager.register(x_coordinate, y_coordinate, ticket_type)
+        return None
