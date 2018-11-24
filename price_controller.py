edfc980e852d700fa249d1545807765f9bb71d47
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2018-11-24 21:11:10 +0100

    Remove useless code

--- price_controller.py
+++ price_controller.py
@@ -3,11 +3,7 @@ from database import Database
 from flask import jsonify
 
 class PriceController:
-
-    def __init__(self, price_manager):
-        self.price_manager = price_manager
-        self.connection = Database()
-
+ 
     def get_prices(self):
         prices = self.connection.get_prices()
         return jsonify(prices)
