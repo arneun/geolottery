51719e3bdbd4a97ee1e9f21510ea84fa72ca67bf
Burdzi0 <burdzi0@programmer.net>
2018-11-24 15:04:49 +0100

    Create CountryChecker which checks if given lat and long is in Poland

---
+++ map/country_checker.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+
+class CountryChecker:
+
+    def __init__(self):
+        print("Created object")
+
+    def check_if_in_poland(self, lattitude, longittiude):
+        address = "https://nominatim.openstreetmap.org/reverse?format=json"
+        concat = "&"
+        lat = "lat=" + str(lattitude)
+        long = "lon=" + str(longittiude)
+
+        address = address + concat + lat + concat + long
+        response = requests.get(address)
+
+        response_json = json.loads(response.content)
+
+        if response_json['address']['country_code'] == 'pl':
+            return True
+        return False
+
