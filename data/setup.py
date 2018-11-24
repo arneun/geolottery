4c0224bfe6b9debdeafa88042f6ae0f1cc97c09a
Burdzi0 <burdzi0@programmer.net>
2018-11-24 17:24:19 +0100

    Fix setup

--- data/setup.py
+++ data/setup.py
@@ -1,11 +1,12 @@
 
 import sqlite3
-from user import User
+from user.user import User
 
 
 class Database:
-    def setup():
-        conn = sqllite3.connect('/home/.geolottery_storage')
+
+    def setup(self):
+        conn = sqlite3.connect('/home/.geolottery_storage')
         c = conn.cursor()
         c.execute('''CREATE TABLE user (name text, mail text, password text) ''')
         conn.commit()
@@ -14,18 +15,16 @@ class Database:
         
         c.execute('''CREATE TABLE prizes (lottery_time text, prize integer ) '''  )
         conn.commit()
-        
 
         conn.close()
     
-    def authenticate(user_name, user_password):
-        conn = sqllite3.connect('/home/.geolottery_storage')
+    def authenticate(self, user_name, user_password):
+        conn = sqlite3.connect('/home/.geolottery_storage')
         c = conn.cursor()
         c.execute('''SELECT name, FROM user WHERE name=? AND mail=? ''', (user_name, user_password) )
         res = c.fetchall()
         conn.commit()
         conn.close()
-        
 
         return User(res[0], res[1], res[2])
         
